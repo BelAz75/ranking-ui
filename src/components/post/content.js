aad0fda07043bb7c19c5ed182ea63e7e82f0062b
Maksim Goncharov <maxim.goncharov@clausematch.com>
2019-09-28 23:00:30 +0300

    Play video

--- src/components/post/content.js
+++ src/components/post/content.js
@@ -15,19 +15,45 @@ class CosmoContent extends Component {
     };
   }
 
+  playVideo () {
+    const video = document.getElementById('video');
+
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+
   render () {
     const { content } = this.state;
 
     return (
       <Cell>
-        {content.videoUrl === null
-        ? <LazyLoadImage
-        width='100%'
-        height='auto'
-        style={{ minHeight: '350px' }}
-        placeholderSrc={placeholderImage}
-        src={content.imageUrl}/>
-        : <div>video</div>}
+        {
+          content.videoUrl === null
+            ? <LazyLoadImage
+              width='100%'
+              height='auto'
+              style={{ minHeight: '350px' }}
+              placeholderSrc={placeholderImage}
+              src={content.imageUrl}/>
+            : <div style={{ position: 'relative' }}>
+              <div
+                style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '1', width: '100%', height: '100%' }}
+                onClick={this.playVideo}>
+              </div>
+
+              <video
+                id='video'
+                src={content.videoUrl}
+                preload='none'
+                type='video/mp4'
+                poster={content.imageUrl}
+                style={{ width: '100%', height: 'auto' }}>
+              </video>
+            </div>
+        }
       </Cell>
     )
   }
